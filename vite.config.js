1207924eadd6e00b5c34f4a064845446ae116a8a
Ashly <thegirlnextfloor@gmail.com>
2026-02-20 03:14:32 -0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  return {
-    base: mode === "production" 
-      ? "/keep-stickyboard/" 
-      : "./",
-    plugins: [react()],
-    server: {
-      port: 5173,
-      strictPort: true,
-    },
-  };
-});
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/keep-stickyboard/" : "/",
+  plugins: [react()],
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
+}));
